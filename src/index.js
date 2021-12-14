c4ca8b79ae6a1b6a317d276551ad8e376b0e7e84
Yago Pajariño <yago977@hotmail.com>
2021-12-14 13:00:49 -0300

    home created

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import home from "./home.js"
 import './style.css';
 
 page_load()
-
 function insertarAntesDelFooter(nodo){
     let footer = document.querySelector("#footer")
     let contenedor = document.querySelector("#content")
     let tab = document.querySelector(".tab")
-    tab.remove()
+    if (tab != undefined){
+        tab.remove()
+    }
     contenedor.insertBefore(nodo,footer)
 }
+
+insertarAntesDelFooter(home())
